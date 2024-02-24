fb181e60b97260e9f7d977a97809cfc5ddaefa84
supertorpe <supertorpe@hotmail.com>
2024-02-24 21:35:51 +0100

    Initial commit

---
+++ docs/assets/status-tap-pzWCl_h8.js
@@ -0,0 +1,4 @@
+import{Z as r,$ as i,a0 as c,a1 as d,a2 as l}from"./vendor-D-c0f6XP.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
+//# sourceMappingURL=status-tap-pzWCl_h8.js.map
