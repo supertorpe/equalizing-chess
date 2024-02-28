0302a41529a0fbaff9b9645ad02b3d96f56e5ad0
supertorpe <supertorpe@hotmail.com>
2024-02-28 01:00:33 +0100

    publish updates

--- docs/assets/status-tap-CsqRkOXz.js
+++ docs/assets/status-tap-CsqRkOXz.js
@@ -1,4 +1,4 @@
-import{a0 as r,a1 as i,a2 as c,a3 as d,a4 as l}from"./vendor-DKy_A90L.js";/*!
+import{a1 as r,a2 as i,a3 as c,a4 as d,a5 as l}from"./vendor-Bx04Omqf.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
-//# sourceMappingURL=status-tap-eGlBhvoR.js.map
+//# sourceMappingURL=status-tap-CsqRkOXz.js.map
